49ae08d04bfa9bc88f21ef20577e208779a8e674
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-05-11 09:04:27 -0400

    Feat: add parent and child shape classes

--- index.js
+++ index.js
@@ -1,38 +1,48 @@
 const inquirer = require('inquirer')
+const { Circle, Triangle, Square } = require('./lib/shapes.js')
 
 // Prompt user questions
+const questions = [
+	{
+		message: 'Please enter the text you would like to include in your logo. Maximum of (3) characters.',
+		name: 'text',
+		type: 'input',
+	},
+	{
+		message: "What color would you like the text to be displayed? (Please enter a color keyword or a color's hexadecimal number.)",
+		name: 'textColor',
+		type: 'input',
+	},
+	{
+		message: 'Please select the shape you would like to include in your logo from the options below:',
+		choices: ['Circle', 'Triangle', 'Square'],
+		name: 'shape',
+		type: 'list',
+	},
+	{
+		message: "What color would you like the shape to be displayed? (Please enter a color keyword or a color's hexadecimal number.)",
+		name: 'shapeColor',
+		type: 'input',
+	},
+]
+
 inquirer
-	.prompt([
-		{
-			message: 'Please enter the text you would like to include in your logo. Maximum of (3) characters.',
-			name: 'text',
-			type: 'input',
-		},
-		{
-			message: "What color would you like the text to be displayed? (Please enter a color keyword or a color's hexadecimal number.)",
-			name: 'textColor',
-			type: 'input',
-		},
-		{
-			message: 'Please select the shape you would like to include in your logo from the options below:',
-			choices: ['Circle', 'Triangle', 'Square'],
-			name: 'shape',
-			type: 'list',
-		},
-		{
-			message: "What color would you like the shape to be displayed? (Please enter a color keyword or a color's hexadecimal number.)",
-			name: 'shapeColor',
-			type: 'input',
-		},
-	])
+	.prompt(questions)
 	.then(answers => {
-        
-    })
+		const { shape } = answers
+		let svgString
 
-
-
-
-// Exports
-module.exports = {
-	inquirer,
-}
+		if (shape === 'Circle') {
+			svgString = new Circle(answers)
+		} else if (shape === 'Triangle') {
+			svgString = new Triangle(answers)
+		} else if (shape === 'Square') {
+			svgString = new Square(answers)
+		} else {
+			console.error('Invalid shape')
+		}
+		return svgString.render()
+	})
+	.then(svgString => {
+		console.log(svgString)
+	})
